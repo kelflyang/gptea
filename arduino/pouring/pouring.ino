a84eee33787def9a8b64cf97a3bc4e3dd5c0e1d0
Kevin <kevintang1509@gmail.com>
2024-05-16 14:59:24 -0400

    working pouring

--- arduino/pouring/pouring.ino
+++ arduino/pouring/pouring.ino
@@ -63,32 +63,27 @@ void setup() {
    ;  // wait for serial port to connect. Needed for native USB port only
  }
 
+ pinMode(12, OUTPUT);
+ digitalWrite(12, LOW);
+
  //Motor Set Up
  pinMode(EN_PIN, OUTPUT);
  digitalWrite (EN_PIN, LOW);
  rotation.setMaxSpeed(300);
  rotation.setAcceleration(300);
-  
-  pinMode(8, INPUT); //Hall Effect  
-  pinMode(2, OUTPUT);  //Haptic Motor
 
-  pinMode(12, OUTPUT);
-  digitalWrite(12, LOW);
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////
 
-
 void loop() {
-  int potValue = analogRead(A5);
-
-  //0. pouring SETUP
   static bool ledShouldBeOn = false;  // Flag to control the LED state
   int pourAmount = 0;                 // Variable to control the amount of each pour
   static int pourCount = 0;           // Total number of pours to perform
-   // Update pourAmount based on pourCount
+
+  // Update pourAmount based on pourCount
   if (pourCount == 0) {
     ledShouldBeOn = true;  // Activate LED when no pours have been performed
   } else if (pourCount == 1) {
@@ -107,107 +102,17 @@ void loop() {
     ledShouldBeOn = true;  // Reset the LED control flag
     return;  // Exit the loop to start over
   }
+
   // Update LED state
   digitalWrite(12, ledShouldBeOn ? HIGH : LOW);
 
-
-
-  
-  // 1. Cup Coaster Dial
-  long potVal = map(potValue, 0, 1023, 0, 10);
-  if (potVal % 2 == 0){
-   if (currentPotVal != potVal){
-     Serial.println(potVal);
-     hapticBuzz();
-     currentPotVal = potVal;
-   }
-  }
-
-
-  // 2. Manual Motor control
-  int result = checkEncoder();
-  if (result == 1) {
-    Serial.println("CW");
-    rotation.move(50);
-  } else if (result == -1) {
-    Serial.println("CCW");
-    rotation.move(-50);
-  }
-  rotation.run();
-  delay(10);
-
-
-  // 3. Pouring: Check if data is available to read from the serial buffer.
-  if (Serial.available() > 0) {
-    // Read the incoming line as a string.
-    String data = Serial.readStringUntil('\n');
-    data.trim();
-    int receivedNumber = data.toInt();
-
-    // Check if the received number is 5.
-    if (receivedNumber == 5) {
-      Serial.println("run pour tea sequence ");
-      pour(pourAmount);
-      pourCount++;
-      delay(1000);
-    }
-  }
-
-  bool hall = digitalRead(8);  //Hall effect; recording button
-
-
-  // Check if magnet is detached and we're not already recording
-  if (hall == HIGH && recordingState == 0) {  //HIGHT = DETACHED
-    Serial.println("Recording audio...");
-    Serial.println("1");
-    recordingState = 1; // Set recording state
-  } 
-  // Check if magnet is attached and stop is not already enabled
-  else if (hall == LOW && recordingState == 1) {
-    Serial.println("Stop recording");
-    Serial.println("3");
-    Serial.println("submitting audio to API");
-    recordingState = 0; // Reset recording state
-  }
-
-  // Reset stopState when hall is HIGH and we are already recording
-  if (recordingState == 1) {
-    //Serial.println("...");
-  }
-
+  // Let's pour it
+  pour(pourAmount);
+  pourCount++;
+  delay(5000); // Delay between pours
 }
 
 
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////
-
-int checkEncoder() {
-  currentPosition = encoder.read();
-
-  // Store the current position in the readings buffer
-  lastReadings[index] = currentPosition;
-  index = (index + 1) % 5; // Move index and wrap around
-
-  // Calculate the average of the last readings
-  long averagePosition = 0;
-  for (int i = 0; i < 5; i++) {
-    averagePosition += lastReadings[i];
-  }
-  averagePosition /= 5;
-
-  // Check if there's a significant change
-  if (averagePosition > previousPosition + 4) { // Threshold for clockwise
-    previousPosition = averagePosition;
-    return 1;
-  } else if (averagePosition < previousPosition - 4) { // Threshold for counterclockwise
-    previousPosition = averagePosition;
-    return -1;
-  }
-  return 0; // No significant change
-}
 
 
 
